633cf6484369c555303ead219eb14a64f2606b9d
MichaelSukkiSong <sa77irak@gmail.com>
2023-11-11 21:34:58 -0800

    added icons to nav buttons, removed @variants in custom styles in tailwind

--- src/components/MainPanel.js
+++ src/components/MainPanel.js
@@ -22,6 +22,25 @@ import {
 } from '@chakra-ui/react';
 import { Button } from '@chakra-ui/react';
 import { useDisclosure } from '@chakra-ui/react';
+import { Icon } from '@chakra-ui/icons';
+import { RiComputerLine, RiMoneyDollarCircleLine } from 'react-icons/ri';
+import { AiOutlineHeart } from 'react-icons/ai';
+import { LuGraduationCap } from 'react-icons/lu';
+import { CiDeliveryTruck } from 'react-icons/ci';
+import { BsMic } from 'react-icons/bs';
+import { BsCart2 } from 'react-icons/bs';
+import { SiHiveBlockchain } from 'react-icons/si';
+import { GiArtificialHive } from 'react-icons/gi';
+import { PiBowlFoodBold } from 'react-icons/pi';
+import { RiGovernmentLine } from 'react-icons/ri';
+import { PiDnaBold } from 'react-icons/pi';
+import { GrInsecure } from 'react-icons/gr';
+import { GoLaw } from 'react-icons/go';
+import { AiOutlineInsurance } from 'react-icons/ai';
+import { PiTelegramLogo } from 'react-icons/pi';
+import { TiWeatherPartlySunny } from 'react-icons/ti';
+import { TbBuildingEstate } from 'react-icons/tb';
+import { LiaRobotSolid } from 'react-icons/lia';
 
 const MainPanel = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
@@ -121,91 +140,86 @@ const MainPanel = () => {
             </div>
           </div>
           <div className="flex items-center my-2 mb-4 ml-2 space-x-4">
-            <div className="flex-auto flex items-center justify-center overflow-hidden">
+            <div className="flex items-center justify-center overflow-hidden">
               <div className="flex space-x-4 lg:space-x-8 overflow-x-scroll hide-scrollbar ">
                 <button className="flex flex-none items-center space-x-2 p-2 rounded-full text-xs font-medium text-gray-600 bg-white border">
-                  icon
+                  <Icon as={RiComputerLine} />
                   <span>Computer Software</span>
                 </button>
                 <button className="flex flex-none items-center space-x-2 p-2 rounded-full text-xs font-medium text-gray-600 bg-white border">
-                  icon
+                  <Icon as={AiOutlineHeart} />
                   <span>Health & Wellness</span>
                 </button>
                 <button className="flex flex-none items-center space-x-2 p-2 rounded-full text-xs font-medium text-gray-600 bg-white border">
-                  icon
+                  <Icon as={RiMoneyDollarCircleLine} />
                   <span>Financial Technology</span>
                 </button>
                 <button className="flex flex-none items-center space-x-2 p-2 rounded-full text-xs font-medium text-gray-600 bg-white border">
-                  icon
-                  <span>Health & Wellness</span>
-                </button>
-                <button className="flex flex-none items-center space-x-2 p-2 rounded-full text-xs font-medium text-gray-600 bg-white border">
-                  icon
+                  <Icon as={LuGraduationCap} />
                   <span>Education</span>
                 </button>
                 <button className="flex flex-none items-center space-x-2 p-2 rounded-full text-xs font-medium text-gray-600 bg-white border">
-                  icon
+                  <Icon as={CiDeliveryTruck} />
                   <span>Transportation</span>
                 </button>
                 <button className="flex flex-none items-center space-x-2 p-2 rounded-full text-xs font-medium text-gray-600 bg-white border">
-                  icon
+                  <Icon as={BsMic} />
                   <span>Media & Entertainment</span>
                 </button>
                 <button className="flex flex-none items-center space-x-2 p-2 rounded-full text-xs font-medium text-gray-600 bg-white border">
-                  icon
+                  <Icon as={BsCart2} />
                   <span>E-Commerce</span>
                 </button>
                 <button className="flex flex-none items-center space-x-2 p-2 rounded-full text-xs font-medium text-gray-600 bg-white border">
-                  icon
+                  <Icon as={SiHiveBlockchain} />
                   <span>Blockchain</span>
                 </button>
                 <button className="flex flex-none items-center space-x-2 p-2 rounded-full text-xs font-medium text-gray-600 bg-white border">
-                  icon
+                  <Icon as={GiArtificialHive} />
                   <span>AI/ML</span>
                 </button>
                 <button className="flex flex-none items-center space-x-2 p-2 rounded-full text-xs font-medium text-gray-600 bg-white border">
-                  icon
+                  <Icon as={PiBowlFoodBold} />
                   <span>Food</span>
                 </button>
                 <button className="flex flex-none items-center space-x-2 p-2 rounded-full text-xs font-medium text-gray-600 bg-white border">
-                  icon
+                  <Icon as={AiOutlineHeart} />
                   <span>Non-Profit</span>
                 </button>
                 <button className="flex flex-none items-center space-x-2 p-2 rounded-full text-xs font-medium text-gray-600 bg-white border">
-                  icon
+                  <Icon as={RiGovernmentLine} />
                   <span>Government</span>
                 </button>
                 <button className="flex flex-none items-center space-x-2 p-2 rounded-full text-xs font-medium text-gray-600 bg-white border">
-                  icon
+                  <Icon as={PiDnaBold} />
                   <span>Biotechnology</span>
                 </button>
                 <button className="flex flex-none items-center space-x-2 p-2 rounded-full text-xs font-medium text-gray-600 bg-white border">
-                  icon
+                  <Icon as={GrInsecure} />
                   <span>Cybersecurity</span>
                 </button>
                 <button className="flex flex-none items-center space-x-2 p-2 rounded-full text-xs font-medium text-gray-600 bg-white border">
-                  icon
+                  <Icon as={GoLaw} />
                   <span>Legal</span>
                 </button>
                 <button className="flex flex-none items-center space-x-2 p-2 rounded-full text-xs font-medium text-gray-600 bg-white border">
-                  icon
+                  <Icon as={AiOutlineInsurance} />
                   <span>Insurance</span>
                 </button>
                 <button className="flex flex-none items-center space-x-2 p-2 rounded-full text-xs font-medium text-gray-600 bg-white border">
-                  icon
+                  <Icon as={PiTelegramLogo} />
                   <span>Telecommunications</span>
                 </button>
                 <button className="flex flex-none items-center space-x-2 p-2 rounded-full text-xs font-medium text-gray-600 bg-white border">
-                  icon
+                  <Icon as={TiWeatherPartlySunny} />
                   <span>Climate Tech</span>
                 </button>
                 <button className="flex flex-none items-center space-x-2 p-2 rounded-full text-xs font-medium text-gray-600 bg-white border">
-                  icon
+                  <Icon as={TbBuildingEstate} />
                   <span>Real Estate & Construction</span>
                 </button>
-
                 <button className="flex flex-none items-center space-x-2 p-2 rounded-full text-xs font-medium text-gray-600 bg-white border">
-                  icon
+                  <Icon as={LiaRobotSolid} />
                   <span>Robotics</span>
                 </button>
               </div>
