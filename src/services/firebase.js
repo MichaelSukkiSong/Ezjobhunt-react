7d373d8252e4a08b7524af956ca3d47fa7db5835
MichaelSukkiSong <sa77irak@gmail.com>
2023-11-26 17:34:40 -0800

    added firebase sdk

---
+++ src/services/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCWDK4Dczs1KBhnmuZSH1UbvlnbYFuXpsY',
+  authDomain: 'ezjobhunt-6a34f.firebaseapp.com',
+  projectId: 'ezjobhunt-6a34f',
+  storageBucket: 'ezjobhunt-6a34f.appspot.com',
+  messagingSenderId: '759583787906',
+  appId: '1:759583787906:web:d769e9dfc76dc3d71a9d9b',
+  measurementId: 'G-7HR8RXDNDQ',
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
