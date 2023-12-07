b12c1434a313648a7aeefa226bebf9cae76d75ff
MichaelSukkiSong <sa77irak@gmail.com>
2023-12-07 09:01:15 -0800

    changed bannersbar spacing

--- src/components/panel/main/Bannersbar.js
+++ src/components/panel/main/Bannersbar.js
@@ -21,7 +21,7 @@ const Bannersbar = () => {
   return (
     <div className="flex items-center my-2 mb-4 ml-2 space-x-4">
       <div className="flex items-center justify-center overflow-hidden">
-        <div className="flex space-x-4 lg:space-x-8 overflow-x-scroll hide-scrollbar ">
+        <div className="flex space-x-4 lg:space-x-4 overflow-x-scroll hide-scrollbar ">
           <button className="flex flex-none items-center space-x-2 p-2 rounded-full text-xs font-medium text-gray-600 bg-white border">
             <Icon as={RiComputerLine} />
             <span>Computer Software</span>
